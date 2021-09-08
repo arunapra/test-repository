0c98487512f535d771e30bcf764d7a86343f0d57
Aruna Pradeepa <aruna.pradeepa@gmail.com>
2021-09-08 13:32:25 +0530

    Changed the database url from postgres to postgresql

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ from resources.item import Item,ItemList
 from resources.store import Store,StoreList
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL','sqlite:///data.db')
+uri=os.environ.get('DATABASE_URL','sqlite:///data.db')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI']=uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key="aruna"
 api=Api(app)
